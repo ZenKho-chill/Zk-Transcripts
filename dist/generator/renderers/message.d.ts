c2d44797bdb96e41b9e6ccf72d2e541ba688b5c3
ZenKho <heruhoangphuc0601@gmail.com>
2025-08-10 21:02:50 +0700

    feat: implement renderMessage function and associated types for message rendering

---
+++ dist/generator/renderers/message.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import type { Message } from "discord.js";
+import type { RenderMessageContext } from "..";
+
+export default function renderMessage(
+  message: Message,
+  context: RenderMessageContext
+): Promise<JSX.Element | undefined>;
